895ff6728c531a0cbf1da17879b83f4805656721
Morthiner <vhbrunetto@hotmail.com>
2022-05-23 11:38:16 -0300

    Novas modalidades

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,10 +1,28 @@
 from replit import clear
 import random
 from hangman_art import estagios, logo
-from hangman_words import lista_paises
+from hangman_words import paises_europa, paises_mundo, paises_asia
+
+modo_jogo = 0
+modalidade = []
+
+while modo_jogo < 1 or modo_jogo > 3:
+    clear()
+    modo_jogo = int(input(" Modalidades:\n\n 1 - Países do mundo\n 2 - Países da europa\n 3 - Países da ásia\n\n> "))
+    print("-"*50)
+    if modo_jogo == 1:
+        modalidade = paises_mundo
+    elif modo_jogo == 2:
+        modalidade = paises_europa
+    elif modo_jogo == 3:
+        modalidade = paises_asia
+    else:
+        print("\nModalidade inválida. Tente novamente.\n")
+
+clear()
 
 fim_do_jogo = False
-palavra_escolhida = random.choice(lista_paises)
+palavra_escolhida = random.choice(modalidade)
 tamanho_palavra = len(palavra_escolhida)
 vidas = 6
 
@@ -19,13 +37,15 @@ while not fim_do_jogo:
     chute = input("Escolha uma letra: ").lower()
     
     clear()
-    
+
     if chute in display:
       print("Você já chutou essa letra")
+
     for posicao in range(tamanho_palavra):
         letra = palavra_escolhida[posicao]
         if letra == chute:
             display[posicao] = letra
+
     if chute not in palavra_escolhida:
         print(f"Você chutou {chute}, esta letra não pertence a palavra. Você perdeu uma vida.")
         vidas -= 1
